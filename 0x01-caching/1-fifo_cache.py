3794890f2527fdc4ca2e176d4bff14993d2507dd
belovetech <belovetech@gmail.com>
2023-01-25 00:27:20 +0100

    [Update]: Implement FIFO caching use queue data structure

--- 0x01-caching/1-fifo_cache.py
+++ 0x01-caching/1-fifo_cache.py
@@ -11,18 +11,17 @@ class FIFOCache(BaseCaching):
         """Initialize FIFO caching
         """
         super().__init__()
+        self.queue = []
 
     def put(self, key, item):
         """Assign key and item to the cache system
         """
-        if len(self.cache_data) == self.MAX_ITEMS \
-           and key not in self.cache_data.keys():
-            # Getting first key in dictionary
-            # first_key = list(self.cache_data.keys())[0]
-            first_key = next(iter(self.cache_data))
-            del self.cache_data[first_key]
-            print("DISCARD: {}".format(first_key))
+        if len(self.cache_data) == self.MAX_ITEMS and key not in self.queue:
+            discard = self.queue.pop(0)
+            del self.cache_data[discard]
+            print("DISCARD: {}".format(discard))
         if key and item:
+            self.queue.append(key)
             self.cache_data[key] = item
 
     def get(self, key):
