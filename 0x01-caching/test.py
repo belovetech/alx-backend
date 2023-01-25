e141d520dd52c1948acab464227907bbdf16a35a
belovetech <belovetech@gmail.com>
2023-01-25 13:53:34 +0100

    [Feat]: implement least recent used caching using queue DSA

---
+++ 0x01-caching/test.py
@@ -0,0 +1,12 @@
+#!/usr/bin/python3
+stack = {"A": 1, "B": 2}
+
+if "A" in stack.keys():
+    print("yes")
+
+
+# stack.insert(0, -1)
+# print(stack.pop())
+# print(stack)
+# print(stack.pop())
+# print(stack)
