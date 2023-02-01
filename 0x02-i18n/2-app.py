1f96772f1027fdb2262aa8b1f26f6a0bef1e16da
belovetech <belovetech@gmail.com>
2023-02-01 07:07:42 +0100

    [Update]: comment out the babel init

--- 0x02-i18n/2-app.py
+++ 0x02-i18n/2-app.py
@@ -19,13 +19,13 @@ app.url_map.strict_slashes = False
 babel = Babel(app)
 
 
-# @babel.localeselector
+@babel.localeselector
 def get_locale():
     """Get locale from request
     """
     return request.accept_languages.best_match(app.config['LANGUAGES'])
 
-babel.init_app(app, locale_selector=get_locale)
+# babel.init_app(app, locale_selector=get_locale)
 
 @app.route('/')
 def say_hello():
--- 0x02-i18n/3-app.py
+++ 0x02-i18n/3-app.py
@@ -19,13 +19,13 @@ app.url_map.strict_slashes = False
 babel = Babel(app)
 
 
-# @babel.localeselector
+@babel.localeselector
 def get_locale():
     """Retrieve request locale
     """
     return request.accept_languages.best_match(app.config['LANGUAGES'])
 
-babel.init_app(app, locale_selector=get_locale)
+# babel.init_app(app, locale_selector=get_locale)
 
 @app.route('/')
 def say_hello():
